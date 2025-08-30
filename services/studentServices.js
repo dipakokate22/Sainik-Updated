42cde6d7e18f56c8f78971801fa6fc6a8a847879
Ayush Tiwari <97778058+Morbiusss@users.noreply.github.com>
2025-08-30 17:06:51 +0530

    fix: Change updateStudentProfile method from PATCH to POST for profile updates

--- services/studentServices.js
+++ services/studentServices.js
@@ -33,7 +33,7 @@ export async function updateStudentProfile(id, payload) {
     });
   } else {
     res = await fetch(`https://sainik.codekrafters.in/api/student/profile`, {
-      method: 'PATCH',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`,
